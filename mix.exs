44445e8d01178b743dea1658859f4794a9b621a8
Harrison Tan <harrisontanh@gmail.com>
2017-06-25 12:06:09 -0400

    Update mix.exs with metadata and ecto dependency

--- mix.exs
+++ mix.exs
@@ -7,27 +7,30 @@ defmodule EctoConditionals.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :ecto]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    EctoConditionals implements a flexibly functional find_or_create and upsert behavior for Ecto models.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["h@rrison.us"],
+      licenses: ["GPL 3.0"],
+      links: %{"GitHub" => "https://github.com/codeanpeace/ecto_conditionals"}
+    ]
+  end
+
   defp deps do
-    []
+    [{:ecto, ">= 0.0.0"}]
   end
 end

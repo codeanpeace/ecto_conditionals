0cc43022133e38ad5296996d587e88c6e96f6f73
Harrison Tan <harrisontanh@gmail.com>
2017-06-25 11:56:24 -0400

    Initial commit

---
+++ lib/ecto_conditionals.ex
@@ -0,0 +1,18 @@
+defmodule EctoConditionals do
+  @moduledoc """
+  Documentation for EctoConditionals.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoConditionals.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
